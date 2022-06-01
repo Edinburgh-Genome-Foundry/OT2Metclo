a297c6b7014cd8dbbc9f93963ff912c7ddee01d9
Dany Matute <daniella.l.matute@gmail.com>
2022-06-01 17:19:54 +0100

    Edited the labware, reagents and added protocol to make reaction

--- ot2metclo.py
+++ ot2metclo.py
@@ -17,9 +17,8 @@ def run(protocol: protocol_api.ProtocolContext):
     thermocycler = protocol.load_module('thermocycler')
     tc_plate = thermocycler.load_labware('biorad_96_wellplate_200ul_pcr')
     thermocycler.set_temperature(4)
-        
     tr_20 = protocol.load_instrument('opentrons_96_tiprack_20ul','1')
-    left_pipette = protocol.load_instrument('p20_single', 'left', tip_racks=tr_20)
+    lpipette = protocol.load_instrument('p20_single', 'left', tip_racks=tr_20)
     reagent_plate = protocol.load_labware('nest_96_wellplate_200ul_flat','2')
 
 ################################################################################
@@ -28,6 +27,8 @@ def run(protocol: protocol_api.ProtocolContext):
     ligase_buffer = reagent_plate.wells('A1')
     ligase = reagent_plate.wells('B1')
     bsai = reagent_plate.wells('C1')
+    #should I add a water trough? or tube? I need to calculate how much water would be 
+    # needed because I dont think a 200ul well will be enough     
     water = reagent_plate.wells('D1')
     #The way of inserting the inserts would need to change further on 
     insert1 = reagent_plate.wells('A2')
@@ -35,19 +36,28 @@ def run(protocol: protocol_api.ProtocolContext):
     insert3 = reagent_plate.wells('C2')
     Assembly_v = reagent_plate.wells('H1')
     
-    
-    thermocycler.open_lid()
-    thermocycler.set_lid_temperature(4) #the lid should be slightly higher than the highest temp 
+################################################################################
+# PROTOCOL
+################################################################################
+    thermocycler.set_lid_temperature(4) 
+    thermocycler.set_block_temperature(4)
 
-    thermocycler.set_block_temperature(37, hold_time_seconds=900, block_max_volume=20)
-    thermocycler.close_lid()
-    #they are updating this. the next release 6.0.0 is expected to solve this issue
-    profile = [
-        {'temperature': 37, 'hold_time_seconds': 120},
-        {'temperature': 16, 'hold_time_seconds': 300},
-        {'temperature': 37, 'hold_time_seconds': 1200},
-        {'temperature': 80, 'hold_time_seconds': 300}
-    ]
-    thermocycler.execute_profile(steps=profile, repetitions = 45, block_max_volume=20) 
-    thermocycler.deactivate()
-    thermocycler.open_lid()
+#Making a metclo assembly reaction for one insert
+#volumes are not a priority right now
+
+    lpipette.pick_up_tip()
+    lpipette.transfer(2,reagent_plate()['A1'], tc_plate()['A1']) #ligase buffer
+    # volume is too small for the pipette
+    lpipette.transfer(0.5,reagent_plate()['B1'], tc_plate()['A1']) #ligase
+    lpipette.transfer(0.5,reagent_plate()['C1'], tc_plate()['A1']) #bsaI
+
+    lpipette.transfer(3,reagent_plate()['A2'], tc_plate()['A1']) #insert1
+    lpipette.transfer(2,reagent_plate()['B2'], tc_plate()['A1']) #insert2
+    lpipette.transfer(2.5,reagent_plate()['C2'], tc_plate()['A1']) #insert3
+
+    lpipette.transfer(2.5,reagent_plate()['H1'], tc_plate()['A1']) #Assembly vector
+
+    lpipette.transfer(7,reagent_plate()['D1'], tc_plate()['A1'])  #water
+
+    
+
