b77a7cd5fc0eb734f499d52eabd69eca0a18ea01
DanyMatute <daniella.l.matute@gmail.com>
2022-06-01 16:37:50 +0100

    added labware and reagents

--- ot2metclo.py
+++ ot2metclo.py
@@ -9,21 +9,36 @@ metadata = {
 }
 
 def run(protocol: protocol_api.ProtocolContext):
-    #Labware
+
+################################################################################
+# LABWARE
+# nest_96_wellplate_100ul_pcr, is placed on the top of the TempDeck 
+################################################################################
     thermocycler = protocol.load_module('thermocycler')
-    tc_plate = thermocycler.load_labware('nest_96_wellplate_100ul_pcr_full_skirt')
+    tc_plate = thermocycler.load_labware('biorad_96_wellplate_200ul_pcr')
+    thermocycler.set_temperature(4)
+        
+    tr_20 = protocol.load_instrument('opentrons_96_tiprack_20ul','1')
+    left_pipette = protocol.load_instrument('p20_single', 'left', tip_racks=tr_20)
+    reagent_plate = protocol.load_labware('nest_96_wellplate_200ul_flat','2')
 
-    plate = protocol.load.labware('nest_96_wellplate_100ul_pcr_full_skirt',1)
-
-    left_pipette = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack])
-
-    #pipetts
-   
-    #command
+################################################################################
+# REAGENTS
+################################################################################
+    ligase_buffer = reagent_plate.wells('A1')
+    ligase = reagent_plate.wells('B1')
+    bsai = reagent_plate.wells('C1')
+    water = reagent_plate.wells('D1')
+    #The way of inserting the inserts would need to change further on 
+    insert1 = reagent_plate.wells('A2')
+    insert2 = reagent_plate.wells('B2')
+    insert3 = reagent_plate.wells('C2')
+    Assembly_v = reagent_plate.wells('H1')
+    
+    
     thermocycler.open_lid()
-    #the lid should be slightly higher than the highest temp
-    thermocycler.set_lid_temperature(85) #the lid should be slightly higher than the highest temp 
-    #20ul of raction will be exposed to 37C for 15 min
+    thermocycler.set_lid_temperature(4) #the lid should be slightly higher than the highest temp 
+
     thermocycler.set_block_temperature(37, hold_time_seconds=900, block_max_volume=20)
     thermocycler.close_lid()
     #they are updating this. the next release 6.0.0 is expected to solve this issue
