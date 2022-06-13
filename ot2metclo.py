8a09456a801ca3f1ad4449eda6979f095385b3f8
Dany Matute <daniella.l.matute@gmail.com>
2022-06-13 15:19:52 +0100

    (1)Allowed multiple parts to be inputed via .txt, (2)Calculated the reagents needed for the reaction, (3) finds the volume of the parts needed for the reaction, (4)made the code more compact

--- ot2metclo.py
+++ ot2metclo.py
@@ -1,6 +1,5 @@
-import profile
+import profile, string
 from opentrons import protocol_api
-#import pandas as pd
 
 ################################################################################
 # METHODS
@@ -19,11 +18,32 @@ def __getparts__ (file):
             parts[name]=[float(ngul),int(bp),volume] 
     return(parts)
 
+def __calcreagents__ (parts, as_s):
+    sum_parts = round(sum((parts[t][2]) for t in parts),3)
+    ligase_buffer = 2.0
+    ligase = 0.5
+    bsai = 1.0 if as_s > 30000 else 0.5
+    water = round(20 - sum([ligase_buffer,ligase, bsai, sum_parts]),3) if sum([ligase_buffer,ligase, bsai, sum_parts]) < 20 else 0
+    reagents = {}
+    for v in ['ligase_buffer','ligase','bsai','water']:
+        reagents[v] = eval(v) 
+    return(reagents)
 
+parts = __getparts__('/home/dany/data/software/GitHub/metclo/test_assembly_parts.txt') | __getparts__('/home/dany/data/software/GitHub/metclo/test_assembly_vector.txt')
+with open('/home/dany/data/software/GitHub/metclo/test_assembly_assembly.txt') as f:
+    assemblysize = int(f.read())
 
+reagents = __calcreagents__ (parts, assemblysize)
+
+alpha = dict(zip(range(0,8),string.ascii_uppercase))
+
+print('PARTS name:[ng/ul, bp, volume]\n',parts, '\nREAGENTS reagent:[volume]', reagents, '\nASSEMBLY SIZE', assemblysize )
+################################################################################
+# PROTOCOL
+################################################################################
 
 metadata = {
-    'apiLevel': '2.0',
+    'apiLevel': '2.1',
     'protocolName': 'Metclo Assembly - hardcoded 6 part assembly',
     'author': 'Daniella Matute <daniella.l.matute@gmail.com',
     'description':'OT-2 protocol that allows for methylase DNA assembly'
@@ -40,7 +60,6 @@ def run(protocol: protocol_api.ProtocolContext):
     tr_20 = protocol.load_labware('opentrons_96_tiprack_20ul', 1)
     lpipette = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=[tr_20])
     reagent_plate = protocol.load_labware('nest_96_wellplate_200ul_flat', 4)
-
     tc_mod.set_lid_temperature(4) 
     tc_mod.set_block_temperature(4)
     tc_mod.open_lid()
@@ -49,67 +68,42 @@ def run(protocol: protocol_api.ProtocolContext):
 # REAGENTS
 ################################################################################
     #Reagents
-    ligase_buffer = reagent_plate.wells('A1')
-    ligase = reagent_plate.wells('B1')
-    bsai = reagent_plate.wells('C1')
-    water = reagent_plate.wells('D1')
+    globals()['ligase_buffer'] = reagent_plate.wells('A1')
+    globals()['ligase'] = reagent_plate.wells('B1')
+    globals()['bsai'] = reagent_plate.wells('C1')
+    globals()['water'] = reagent_plate.wells('D1')
     
-    #Parts. There is a max of 6 parts with p15a and 4 parts with oriF vector plasmids
-    part1 = reagent_plate.wells('A2')
-    part2 = reagent_plate.wells('B2')
-    part3 = reagent_plate.wells('C2')
-    part4 = reagent_plate.wells('D2')
-    part5 = reagent_plate.wells('E2')
-    part6 = reagent_plate.wells('F2')
-
-    #Assembly vectors (usually p-a )
-    Assembly_v = reagent_plate.wells('H2')
+    for key,v in parts.items():
+        n = (list(parts.keys()).index(key))
+        p = alpha[n] + '2'
+        globals()[key]= reagent_plate.wells(p)
 
 ################################################################################
-# Assembly Plan
+# Assembly 
 ################################################################################
 #Creating Master Mix
-    lpipette.transfer(2,ligase_buffer, tc_plate['A1']) 
-    lpipette.transfer(0.5,ligase, tc_plate['A1']) #ligase
-    lpipette.transfer(2,part1, tc_plate['A1']) #part1
-    lpipette.transfer(2,part2, tc_plate['A1']) #part2
-    lpipette.transfer(2,part3, tc_plate['A1']) #part3
-    lpipette.transfer(2,part4, tc_plate['A1']) #part4
-    lpipette.transfer(2,part5, tc_plate['A1']) #part5
-    lpipette.transfer(2,part6, tc_plate['A1']) #part6
-    lpipette.transfer(2,Assembly_v, tc_plate['A1']) #Assembly vector
     
-    #There is an optional step in protocol to ad 1ul of Bsai to assemblies larger than 30kb
-    '''
-    if assemblysize > 30000:
-        lpipette.transfer(1,reagent_plate['C1'], tc_plate['A1']) #bsaI
-        lpipette.transfer(2.5,reagent_plate['D1'], tc_plate['A1'])  #water
-    else:
-        lpipette.transfer(0.5,reagent_plate['C1'], tc_plate['A1']) #bsaI
-        lpipette.transfer(3,reagent_plate['D1'], tc_plate['A1']) 
-    '''
+    for i in parts:
+        protocol.comment('Transferring '+ i)
+        lpipette.transfer(parts[i][2],globals()[i], tc_plate['A1'])
     
-    lpipette.transfer(0.5,bsai, tc_plate['A1']) #bsaI
-    #the master mix should equal 20. This volume is reached with the addition of water
-    #this volume is variable
-    lpipette.transfer(3,water, tc_plate['A1'])  #water
+    for i in reagents:
+        protocol.comment('Transferring '+ i)
+        lpipette.transfer(reagents[i],globals()[i],tc_plate['A1'])
+
 
 #Thermocycler
+    protocol.comment('Assembly reaction ongoing')
     tc_mod.set_lid_temperature(85)
     tc_mod.set_block_temperature(37, hold_time_minutes=15, block_max_volume=20)
     tc_mod.close_lid()
-
-    
     profile = [
         {'temperature':37, 'hold_time_minutes':2},
         {'temperature':16, 'hold_time_minutes':5},
         {'temperature':37, 'hold_time_minutes':20},
         {'temperature':80, 'hold_time_minutes':5}
     ]
-
     tc_mod.execute_profile(steps= profile, repetitions= 45, block_max_volume= 20)
-
     tc_mod.set_lid_temperature(4) 
     tc_mod.set_block_temperature(4)
     protocol.comment('Metclo assembly done. Assembly is incubating at 4 degrees Celsius.')
-
