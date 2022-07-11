fc1fad990e1975d89d08dfd21c066e3679d5bbc7
Dany Matute <daniella.l.matute@gmail.com>
2022-07-11 23:31:26 +0100

    merged the pdf and the plan .py

--- metclo_plan.py
+++ metclo_plan.py
@@ -1,5 +1,8 @@
 import csv
 import numpy as np
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
 
 def __countinstances__(uncompressed_parts):
     count_parts = {}
@@ -23,6 +26,10 @@ def __calcreagents__ (assembly_size, part_volumes):
         reagents[v] = eval(v) 
     return(reagents)
 
+def __fillplate__(items):
+    pass
+def __fillfalcon__(items):
+    pass
 
 assemblies = []
 uncompressed_parts = []
@@ -65,5 +72,23 @@ for i in reagent_sum:
         original = reagent_sum[i]
         reagent_sum[i] = original + ((assembly_dictionary[j])[2][i]*0.25)
 
+def form(path):
+        my_canvas = canvas.Canvas(path, pagesize=letter)
+        my_canvas.setLineWidth(.3)
+        my_canvas.setFont('Helvetica', 15)
+        my_canvas.drawString(30, 750, 'INSTRUCTIONS')
+        my_canvas.drawString(30, 750, 'ASSEMBLIES')
+        my_canvas.drawString(30, 735, 'PARTS')
+        my_canvas.drawString(30, 720, 'ASSEMBLY PLAN')
+        my_canvas.drawString(30, 705, 'MAP')
+        my_canvas.setFont('Helvetica', 11)
+        my_canvas.drawString(40, 690, 'OT2 BENCH LAYOUT')
+        my_canvas.drawString(40, 675, 'PART & REAGENT 96-WELL-PLATE')
+        my_canvas.drawString(40, 660, 'FALCON TUBES')
+        my_canvas.drawString(40, 645, 'ASSEMBLY PLATE 96-WELL-PLATE')
+        my_canvas.save()
+
+#if __name__ == '__main__':
+ #   form('metclo_plan.pdf')
 
 
--- ot2metclo.py
+++ ot2metclo.py
@@ -2,61 +2,6 @@ import profile, string
 from opentrons import protocol_api
 import numpy as np
 
-################################################################################
-# METHODS
-################################################################################
-def __calcvolume__ (ngul, bp):
-    'takes the concentration in ng/ul and sequence lenght of the sample and finds the volume needed containing 30fmol'
-    volume = round(30/((ngul/(1e-6))/(bp*617.96+ 36.04)),3)
-    return volume
-
-def __getparts__ (file):
-    with open(file, 'r') as parts_file:
-        parts={}
-        for l in parts_file:  
-            name, ngul, bp = l.strip().split(',')
-            volume = __calcvolume__(float(ngul),int(bp))
-            parts[name]=[float(ngul),int(bp),volume] 
-    return(parts)
-
-def __calcreagents__ (parts, as_s):
-    sum_parts = round(sum((parts[t][2]) for t in parts),3)
-    ligase_buffer = 2.0
-    ligase = 0.5
-    bsai = 1.0 if as_s > 30000 else 0.5
-    water = round(20 - sum([ligase_buffer,ligase, bsai, sum_parts]),3) if sum([ligase_buffer,ligase, bsai, sum_parts]) < 20 else 0
-    reagents = {}
-    for v in ['ligase_buffer','ligase','bsai','water']:
-        reagents[v] = eval(v) 
-    return(reagents)
-
-def __checkwells__ (reagent_arr):
-    test = np.zeros(8, dtype=bool)
-    print(test)
-    for i in reagent_arr.T:
-        if i == test:
-            print('yay')
-
-alpha = dict(zip(range(0,8),string.ascii_uppercase))
-numb = np.linspace(1,12,12)
-reagent_arr = np.zeros((8,12), dtype=bool)
-constructarr = np.zeros((8,12), dtype=bool)
-
-#parts_file = input("Enter the path of your <parts>.txt:")
-# #assemblysize= int(input("Enter final assembly size (bp):") )
-parts_file = '/home/dany/data/software/GitHub/metclo/test_assembly_parts.txt'
-assemblysize= 800000
-
-
-parts = __getparts__(parts_file) 
-reagents = __calcreagents__ (parts, assemblysize)
-
-print("PARTS ('part_name':[ng/ul, bp, volume])\n",parts, "\n\nREAGENTS ('reagent':volume)", reagents, "\n\nASSEMBLY SIZE", assemblysize )
-__checkwells__(reagent_arr)
-################################################################################
-# PROTOCOL
-################################################################################
-
 metadata = {
     'apiLevel': '2.3',
     'protocolName': 'Metclo Assembly - hardcoded with one assembly that cna change in size',
@@ -69,32 +14,25 @@ def run(protocol: protocol_api.ProtocolContext):
 ################################################################################
 # LABWARE
 # nest_96_wellplate_100ul_pcr, is placed on the top of the TempDeck 
-################################################################################
-    
-    # Instrument
-    p_20 = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=[tr_20])
-    p_300 = protocol.load_instrument('p300_single_gen2', 'right', tip_racks=[tr_300])
-    
+################################################################################    
     # Modules
     temp_module = protocol.load_module("temperature module", 1)
     mag_module = protocol.load_module("magnetic module gen2", 4)
     tc_mod = protocol.load_module("thermocycler module")
-    
     # Labware
     tr_20 = protocol.load_labware('opentrons_96_tiprack_20ul', 9)
     tr_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 6)
     part_plate = protocol.load_labware('nest_96_wellplate_200ul_flat', 2)
     tc_plate = tc_mod.load_labware('biorad_96_wellplate_200ul_pcr')
     falcon = protocol.load_labware('opentrons_15_tuberack_falcon_15ml_conical',5)
-    
+    # Instrument
+    p_20 = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=[tr_20])
+    p_300 = protocol.load_instrument('p300_single_gen2', 'right', tip_racks=[tr_300])
 ################################################################################
 # REAGENTS
 ################################################################################
     #Filling the reagent plate
 
-
-    
-'''
     globals()['ligase_buffer'] = part_plate.wells('A1')
     globals()['ligase'] = part_plate.wells('B1')
     globals()['bsai'] = part_plate.wells('C1')
@@ -138,4 +76,3 @@ def run(protocol: protocol_api.ProtocolContext):
     tc_mod.set_lid_temperature(4) 
     tc_mod.set_block_temperature(4)
     protocol.comment('Metclo assembly done. Assembly is incubating at 4 degrees Celsius.')
-    '''
