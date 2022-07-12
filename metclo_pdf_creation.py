8dac71eaf1e53983428430fe4cf6db4a4e21eba5
Dany Matute <daniella.l.matute@gmail.com>
2022-07-12 22:02:21 +0100

    created .txt to instruct OT2 protocol

--- metclo_pdf_creation.py
+++ metclo_pdf_creation.py
@@ -1,27 +1,38 @@
-#from reportlab.lib.pagesizes import letter
-#from reportlab.pdfgen import canvas
+
+from reportlab.lib.units import inch
+from reportlab.pdfgen.canvas import Canvas
 class pdfcreator:
-    def __init__(self, assembly, parts, reagents):
+    def __init__(self,assembly, parts, reagents):
         self.allassemblies = assembly
         self.allparts = parts
-        self.reagents = reagents'''
+        self.reagents = reagents
 
+    def return_assemblies(self):
+        for i in self.allassemblies:
+            print(i)
 
-    '''def form(path):
-        my_canvas = canvas.Canvas(path, pagesize=letter)
-        my_canvas.setLineWidth(.3)
-        my_canvas.setFont('Helvetica', 15)
-        my_canvas.drawString(30, 750, 'INSTRUCTIONS')
-        my_canvas.drawString(30, 750, 'ASSEMBLIES')
-        my_canvas.drawString(30, 735, 'PARTS')
-        my_canvas.drawString(30, 720, 'ASSEMBLY PLAN')
-        my_canvas.drawString(30, 705, 'MAP')
-        my_canvas.setFont('Helvetica', 11)
-        my_canvas.drawString(40, 690, 'OT2 BENCH LAYOUT')
-        my_canvas.drawString(40, 675, 'PART & REAGENT 96-WELL-PLATE')
-        my_canvas.drawString(40, 660, 'FALCON TUBES')
-        my_canvas.drawString(40, 645, 'ASSEMBLY PLATE 96-WELL-PLATE')
-        my_canvas.save()
+    def createpdf(self):  
+        def form(path):
+            my_canvas = canvas.Canvas(path, pagesize=letter)
+            my_canvas.setLineWidth(.3)
+            my_canvas.setFont('Helvetica', 15)
+            my_canvas.drawString(30, 750, 'INSTRUCTIONS')
+            my_canvas.setFont('Helvetica', 11)
+           # myfile = open("/home/dany/data/software/GitHub/metclo/instructions.txt", "rt")
+           # contents = myfile.read()
+            #myfile.close()  
+            #my_canvas.drawString(30, 735, contents)
+            my_canvas.drawString(30, 750, 'ASSlsEMBLIES')
+            my_canvas.drawString(30, 735, 'PARTS')
+            my_canvas.drawString(30, 720, 'ASSEMBLY PLAN')
+            my_canvas.drawString(30, 705, 'MAP')
+            my_canvas.setFont('Helvetica', 11)
+            my_canvas.drawString(40, 690, 'OT2 BENCH LAYOUT')
+            my_canvas.drawString(40, 675, 'PART & REAGENT 96-WELL-PLATE')
+            my_canvas.drawString(40, 660, 'FALCON TUBES')
+            my_canvas.drawString(40, 645, 'ASSEMBLY PLATE 96-WELL-PLATE')
+            my_canvas.save()
+        if __name__ == '__main__':
+            form('metclo_plan.pdf')
 
-    if __name__ == '__main__':
-        form('metclo_plan.pdf')'''
+        
--- metclo_plan.py
+++ metclo_plan.py
@@ -1,8 +1,6 @@
 import csv
 import numpy as np
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-
+import sys
 
 def __countinstances__(uncompressed_parts):
     count_parts = {}
@@ -26,30 +24,44 @@ def __calcreagents__ (assembly_size, part_volumes):
         reagents[v] = eval(v) 
     return(reagents)
 
-def __fillplate__(items):
-    pass
-def __fillfalcon__(items):
-    pass
+
 
 assemblies = []
 uncompressed_parts = []
-
-with open('/home/dany/Dropbox/EGF/Metclo/Metclo Simulation/DNA Files/oriFCam/finalassembly.csv', newline='') as csvfile:
-    assembly_row = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in assembly_row:
-        list_row = [ele for ele in ((', '.join(row)).split(',')) if ele.strip()]
-        assemblies.append(list_row)
-        for j in list_row:
-            uncompressed_parts.append(j)
+assembly_path = '/home/dany/Dropbox/EGF/Metclo/Metclo Simulation/DNA Files/oriFCam/finalassembly.csv'
+part_path = '/home/dany/Dropbox/EGF/Metclo/Metclo Simulation/DNA Files/oriFCam/parts.csv'
+#assembly_path = input('Input Full Pathway of Assembly (.csv):\n')
+#part_path = input('Input Full Pathway of Parts (.csv):\n')
+try:
+    with open(assembly_path, newline='') as csvfile:
+        assembly_row = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in assembly_row:
+            list_row = [ele for ele in ((', '.join(row)).split(',')) if ele.strip()]
+            assemblies.append(list_row)
+            for j in list_row:
+                uncompressed_parts.append(j)
+    if len(assemblies) > 96:
+        print('Too manny assemblies. Max number of assemblies = 96, <assembly_plan>.csv assemblies = '+ str(len(assemblies)))
+        sys.exit(1)
+except:
+    print('Assembly file error.')
+    sys.exit(1)
 
 parts_con_bp = []
-with open('/home/dany/Dropbox/EGF/Metclo/Metclo Simulation/DNA Files/oriFCam/parts.csv', newline='') as csvfile:
-    part_row = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in part_row:
-        list_row = [ele for ele in ((', '.join(row)).split(',')) if ele.strip()]  
-        parts_con_bp.append(list_row)
+try:
+    with open(part_path, newline='') as csvfile:
+        part_row = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in part_row:
+            list_row = [ele for ele in ((', '.join(row)).split(',')) if ele.strip()]  
+            parts_con_bp.append(list_row)
+except:
+    print('Part file error.')
+    sys.exit(1)
 
 part_count = __countinstances__(uncompressed_parts)
+if len(part_count) > 96:
+    print('Too manny parts. Max number of parts = 96, given number of parts = '+ str(len(part_count)))
+    sys.exit(1)
 
 part_dictionary = {}
 for i in part_count:
@@ -64,7 +76,6 @@ for i in assemblies:
     part_volume_sum = 0
     for j in i[2:]:
         part_volume_sum += round(part_dictionary[j][0],3)
-    #print(i[1])
     assembly_dictionary[i[0]] = [i[1],i[2:],__calcreagents__(int(i[1]),part_volume_sum)]
 
 for i in reagent_sum:
@@ -72,23 +83,14 @@ for i in reagent_sum:
         original = reagent_sum[i]
         reagent_sum[i] = original + ((assembly_dictionary[j])[2][i]*0.25)
 
-def form(path):
-        my_canvas = canvas.Canvas(path, pagesize=letter)
-        my_canvas.setLineWidth(.3)
-        my_canvas.setFont('Helvetica', 15)
-        my_canvas.drawString(30, 750, 'INSTRUCTIONS')
-        my_canvas.drawString(30, 750, 'ASSEMBLIES')
-        my_canvas.drawString(30, 735, 'PARTS')
-        my_canvas.drawString(30, 720, 'ASSEMBLY PLAN')
-        my_canvas.drawString(30, 705, 'MAP')
-        my_canvas.setFont('Helvetica', 11)
-        my_canvas.drawString(40, 690, 'OT2 BENCH LAYOUT')
-        my_canvas.drawString(40, 675, 'PART & REAGENT 96-WELL-PLATE')
-        my_canvas.drawString(40, 660, 'FALCON TUBES')
-        my_canvas.drawString(40, 645, 'ASSEMBLY PLATE 96-WELL-PLATE')
-        my_canvas.save()
-
-#if __name__ == '__main__':
- #   form('metclo_plan.pdf')
-
+with open('data.txt','w') as f:
+    for i in assembly_dictionary:
+        w = "{},{},{},{}\n"
+        f.write(w.format(i, assembly_dictionary[i][0], assembly_dictionary[i][1], assembly_dictionary[i][2]))
+    for i in part_dictionary:
+        w = "{},{},{}\n"
+        f.write(w.format(i, part_dictionary[i][0],part_dictionary[i][1]))
+    for i in reagent_sum:
+        w = "{},{}\n"
+        f.write(w.format(i,reagent_sum[i])) 
 
